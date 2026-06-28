c21de99c6921db7656c44b701f7c030421b000d2
H-DJACK <djkachempire@gmail.com>
2026-06-28 13:57:55 +0200

    Improve manager test form country code search and phone formatting

--- app/routers/web.py
+++ app/routers/web.py
@@ -80,3 +80,9 @@ async def client_open_account_page(request: Request):
 @router.get("/backoffice/packages")
 async def backoffice_packages_page(request: Request):
     return templates.TemplateResponse(request, "backoffice_packages.html", {"request": request})
+
+
+@router.get("/open-account-test")
+async def open_account_test_page(request: Request):
+    return templates.TemplateResponse(request, "client_open_account_manager.html", {"request": request})
+
